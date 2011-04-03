fcee75b6f3ea103f0b640a80325e7eb8f8fd2b2b
Aragaer <aragaer@gmail.com>
2011-04-03 09:36:54 +0400

    dbwrapper should work properly now.

--- dbwrapper.js
+++ dbwrapper.js
@@ -1,4 +1,3 @@
-const Cc = Components.classes;
 const Ci = Components.interfaces;
 
 Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
@@ -6,23 +5,20 @@ Components.utils.import("resource://gre/modules/Services.jsm");
 
 const obs = Services.obs;
 
-function dbwrapper() { }
+var gDB;
+function dbwrapper() {
+    if (!gDB)
+        gDB = this;
+}
 
 dbwrapper.prototype = {
     classDescription:   "EVE Online static dump",
     classID:            Components.ID("{66575bef-61c0-4ea3-9c34-17c10870e6a9}"),
     contractID:         "@aragaer/eve/db;1",
     QueryInterface:     XPCOMUtils.generateQI([Ci.nsIEveDBService, Ci.nsIObserver]),
-    _xpcom_categories: [{
-        category:       'app-startup',
-        entry:          '@aragaer/eve/db;1',
-        service:        true,
-    }],
 
-    _conn:              null,
-
-    getConnection:      function () {dump("conn requested: "+this._conn+"\n"); return this._conn; },
-    createStatement:    function (q) this._conn.createStatement(q),
+    getConnection:      function () gDB._conn,
+    createStatement:    function (q) gDB._conn.createStatement(q),
 
     observe:            function (aSubject, aTopic, aData) {
         switch (aTopic) {
@@ -31,68 +27,57 @@ dbwrapper.prototype = {
             obs.addObserver(this, 'quit-application', false);
             break;
         case 'profile-after-change':
-            this._init();
-            obs.notifyObservers(this._conn, 'eve-db-init', null);
+            var res = init();
+            obs.notifyObservers(gDB._conn, res ? 'eve-db-init' : 'eve-db-error', null);
             break;
         case 'quit-application':
             obs.removeObserver(this, 'profile-after-change');
             obs.removeObserver(this, 'quit-application');
         }
-    },
-
-    _init:              function () {
-        var file = Cc["@mozilla.org/file/directory_service;1"].
-                getService(Ci.nsIProperties).get('ProfD', Ci.nsIFile);
-        file.append('data');
-        if (!file.exists())
-            file.create(file.DIRECTORY_TYPE, 0777);
- 
-        file.append('api.db');
-        if (!file.exists())
-            file.create(file.NORMAL_FILE_TYPE, 0700);
-
-        if (this._conn)
-            this._conn.close();
-
-        this._conn = Cc["@mozilla.org/storage/service;1"].
-                getService(Ci.mozIStorageService).
-                openDatabase(file);
-
-        this._initLocalDB();
-
-        try {
-            this._openStaticDB();
-            dump("Static DB initialized\n");
-        } catch (e) {
-            dump(e.toString()+"\n");
-            dump(this._conn.lastErrorString+"\n");
-        }
-
-        dump("DB service initialized\n");
-    },
-
-    _initLocalDB:       function () {
-        this._conn.executeSimpleSQL("PRAGMA synchronous = OFF");
-        this._conn.executeSimpleSQL("PRAGMA temp_store = MEMORY");
-        if (!this._conn.tableExists('characters'))
-            this._conn.createTable('characters',
-                    'name char, id integer, account integer, ' +
-                    'corporation integer, primary key (id)');
-        if (!this._conn.tableExists('corporations'))
-            this._conn.createTable('corporations',
-                    'name char, ticker char, id integer, ' +
-                    'alliance integer, primary key (id)');
-    },
-
-    _openStaticDB:      function () {
-        var static = Cc["@mozilla.org/preferences-service;1"].
-            getService(Ci.nsIPrefBranch).getCharPref("eve.static_dump_path");
-        dump(static+"\n");
-        this._conn.executeSimpleSQL("attach database '"+static+"' as static\n")
-    },
+    }
 }
 
 var components = [dbwrapper];
 
 var NSGetFactory = XPCOMUtils.generateNSGetFactory(components);
+
+function init() {
+    var file = Services.dirsvc.get('ProfD', Ci.nsIFile);
+    file.append('data');
+    if (!file.exists())
+        file.create(file.DIRECTORY_TYPE, 0777);
+
+    file.append('api.db');
+    if (!file.exists())
+        file.create(file.NORMAL_FILE_TYPE, 0700);
+
+    if (gDB._conn)
+        gDB._conn.close();
+
+    try {
+        gDB._conn = Services.storage.openDatabase(file);
+        dump("DB file "+file.path+" opened\n");
+        initLocalDB();
+        dump("Initalization done\n");
+        openStaticDB();
+        dump("Static DB initialized\n");
+    } catch (e) {
+        dump(e.toString()+"\n");
+        dump(gDB._conn.lastErrorString+"\n");
+        return false;
+    }
+
+    return true;
+}
+
+function initLocalDB() {
+    gDB._conn.executeSimpleSQL("PRAGMA synchronous = OFF");
+    gDB._conn.executeSimpleSQL("PRAGMA temp_store = MEMORY");
+}
+
+function openStaticDB() {
+    var static = Services.prefs.getCharPref("eve.static_dump_path");
+    dump(static+"\n");
+    gDB._conn.executeSimpleSQL("attach database '"+static+"' as static\n")
+}
 
