63fbc18a90c8fb608566b2c2b5679128c97ae75a
Ilya Konovalov <aragaer@gmail.com>
2011-04-04 23:22:08 -0700

    stm will be reset in GetAllCorporations now

--- hr.js
+++ hr.js
@@ -230,7 +230,7 @@ EveHRManager.prototype = {
         } catch (e) {
             dump("getAllCorporations:"+e.toString()+"\n");
         } finally {
-            stm.reset;
+            stm.reset();
         }
         out.value = res.length;
         return res;
