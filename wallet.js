3182847459602a06d4e8105e0612eab495041b9f
Aragaer <aragaer@gmail.com>
2011-04-06 23:50:57 +0400

    Synchronous wallet walking works now

--- wallet.js
+++ wallet.js
@@ -4,9 +4,9 @@ const Ci = Components.interfaces;
 Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
 Components.utils.import("resource://gre/modules/Services.jsm");
 
-const fields = "typeID quantity price timestamp type tFor clientID".split(" ");
+const trans_fields = "typeID quantity price timestamp type tFor clientID".split(" ");
 function transaction(data) {
-    [this['_'+f] = data[f] for each (f in fields)];
+    [this['_'+f] = data[f] for each (f in trans_fields)];
 }
 
 transaction.prototype = {
@@ -24,10 +24,109 @@ transaction.prototype = {
     get clientID()  this._clientID,
 };
 
-var gwm, conn;
+function WalletIntervalObserver(char_id) {
+    this.char_id = char_id;
+    this.start = Date.now();
+    Services.obs.addObserver(this, 'eve-data', false);
+    Services.obs.addObserver(this, 'eve-data-error', false);
+}
+
+WalletIntervalObserver.prototype = {
+    char_id:        null,
+    chainedTrans:   null,
+    error:          false,
+    observe:        function (aSubject, aTopic, aData) {
+        if (aData != 'charwallettrans')
+            return;
+
+        //aSubject contains fetched data now. All we need is to get the date of the last item.
+        var doc = aSubject.wrappedJSObject.doc;
+        var aux = aSubject.wrappedJSObject.aux;
+
+        // now it either has to be the same chain or if the chain has just started - same character
+        if (aux.characterID != this.char_id || aux.beforeTransID != this.chainedTrans)
+            return;
+
+        // now we can assume it is the same chain. Proceed.
+        switch (aTopic) {
+        case 'eve-data':
+            try {
+                var row = doc.evaluate('/eveapi/result/rowset/row[last()]', doc, null, 0, null);
+                res = row.iterateNext();
+                this.chainedTrans = res.getAttribute('transactionID');
+                acquired_start = Date.UTCFromEveTimeString(res.getAttribute('transactionDateTime'));
+            } catch (e) {
+                dump("Error in charwallettrans observer: "+e+"\n");
+                real_start = 0;
+            }
+
+            var stm;
+
+            try {
+                var intervals = [];
+                conn.beginTransaction();
+                stm = gwm.getIntervalsStm2;
+                stm.params.char_id = this.char_id;
+                stm.params.start = acquired_start;
+                stm.params.end = real_end;
+                while (stm.step())
+                    intervals.push({start:stm.row.start_time, end:stm.row.end_time});
+                stm.reset();
+                if (intervals.length > 1) {
+                    stm = gwm.dropIntervalsStm2;
+                    stm.params.start = acquired_start;
+                    stm.params.end = real_end;
+                    stm.params.char_id = this.char_id;
+                    stm.execute();
+                    stm.reset();
+                }
+
+                var new_start, new_end;
+                if (intervals.length) {
+                    // intervals[0] ends inside our new interval, last of intervals starts inside our array
+                    // combine those
+                    new_start = acquired_start < intervals[0].start ? acquired_start : intervals[0].start;
+                    new_end = real_end < intervals[intervals.length-1].end ? real_end : intervals[intervals.length-1].end;
+                } else {
+                    new_start = acquired_start;
+                    new_end = real_end;
+                }
+
+                stm = gwm.storeIntervalStm;
+                stm.params.char_id = this.char_id;
+                stm.params.start = acquired_start;
+                stm.params.end = real_end;
+                stm.execute();
+                stm.reset();
+
+                conn.commitTransaction();
+            } catch (e) {
+                dump("Error in wallettrans database operations: "+e+"\n");
+                dump("Error in wallettrans database operations: "+conn.lastErrorString+"\n");
+                conn.rollbackTransaction();
+            } finally {
+                stm.reset();
+            }
+            break;
+        case 'eve-data-error':
+            if (aux.error.length) {
+                this.error = true;
+                dump(aux.error+"\n");
+            }
+            break;
+        default:
+            dump("Huh?\n");
+            break;
+        }
+    },
+}
+
+var gwm, conn, gEAR, gAM;
 function walletman() {
     if (gwm)
         return;
+    gEAR = Cc["@aragaer/eve/api-requester;1"].getService(Ci.nsIEveApiRequester);
+    gAM = Cc["@aragaer/eve/auth-manager;1"].getService(Ci.nsIEveAuthManager);
     var db = Cc["@aragaer/eve/db;1"].getService(Ci.nsIEveDBService);
     try {
         conn = db.getConnection();
@@ -60,13 +159,15 @@ walletman.prototype = {
                 intervals.push({start: stm.row.start_time, end: stm.row.end_time});
         } catch (e) {
             dump("GetTransactions for char "+char.id+": "+e+"\n");
+        } finally {
+            stm.reset();
         }
 
         var real_start;
 
         switch (intervals.length) {
         case 1:         // everything's within one interval and we got it cached
-            real_start = now();
+            real_start = Date.now();
             break;
         case 0:         // totally uncovered interval. Have to fetch it
             real_start = start_time;
@@ -76,7 +177,52 @@ walletman.prototype = {
             break;
         }
 
-        dump("And now we have to fetch data from "+real_start+" to "+end_time+"\n");
+        dump("And now we have to fetch data from "+Date(start_time)+" to "+Date(real_start)+"\n");
+
+        var tok = null;
+        tok = gAM.getTokenForChar(char, Ci.nsEveAuthTokenType.TYPE_FULL);
+
+        if (!tok) {
+            out.value = 0;
+            return res;
+        }
+
+        var observer = new WalletIntervalObserver();
+        var real_end = observer.start;
+
+        var aux = {wrappedJSObject:{
+                userID:     tok.accountID,
+                apiKey:     tok.apiKey,
+                characterID:tok.characterID,
+            }};
+
+        dump(Date(observer.start)+" (acquired) vs "+Date(start_time)+" (target)\n");
+        while (observer.start > start_time && !observer.error) {
+            gEAR.refreshData('charwallettrans', aux);
+            aux.beforeTransID = observer.chainedTrans;
+            break; // TODO:temporary!
+        }
+        Services.obs.removeObserver(observer, 'eve-data');
+        Services.obs.removeObserver(observer, 'eve-data-error');
+
+        // Now all the data is in the database. Almost win!
+        dump("Everything's in database. Proceed\n");
+
+        let stm = gwm.getTransactionsStm;
+        try {
+            stm.params.char_id = char.id;
+            stm.params.t_for = Ci.nsEveTransactionFor.TYPE_PERSONAL;
+            stm.params.start = start_time;
+            stm.params.end = end_time;
+            while (stm.step())
+                res.push(transaction.fromStmRow(stm.row));
+        } catch (e) {
+            dump(e.toString()+"\n");
+            if (conn)
+                dump(conn.lastErrorString+"\n");
+        } finally {
+            stm.reset();
+        }
 
         out.value = res.length;
         return res;
@@ -127,31 +273,68 @@ function wallet_init() {
             'charID integer, start_time integer, end_time integer');
     gwm.getIntervalsStm = conn.createStatement("select start_time, end_time " +
             "from wallet_cached_intervals where charID=:char_id order by start_time;");
-    gwm.getIntervalsStm2 =  conn.createStatement("select start_time, end_time " +
+    gwm.getIntervalsStm2 = conn.createStatement("select start_time, end_time " +
             "from wallet_cached_intervals where charID=:char_id " +
             "and (start_time < :end or end_time > :start) " +
             "order by start_time;");
+    gwm.dropIntervalsStm2 = conn.createStatement("delete " +
+            "from wallet_cached_intervals where charID=:char_id " +
+            "and (start_time <= :end or end_time >= :start)");
+    gwm.storeIntervalStm = conn.createStatement("insert into wallet_cached_intervals " +
+            "(charID, start_time, end_time) values (:char_id, :start, :end)");
+    gwm.storeTransactionStm = conn.createStatement("replace into wallet_transactions " +
+            "(charID, clientID, typeID, price, type, timestamp, quantity, tFor) " +
+            "values (:char_id, :client, :type_id, :price, :type, :time, :quantity, :t_for);");
+    gwm.getTransactionsStm = conn.createStatement("select " +
+            "charID, clientID, typeID, price, type, timestamp, quantity, tFor " +
+            "from wallet_transactions " +
+            "where charID=:char_id and timestamp between :start and :end " +
+            "and tFor=:t_for;");
 }
+
+const trTypeConversion = {
+    buy:        Ci.nsEveTransactionType.TYPE_BUY,
+    sell:       Ci.nsEveTransactionType.TYPE_SELL,
+    personal:   Ci.nsEveTransactionFor.TYPE_PERSONAL,
+    corporation:Ci.nsEveTransactionFor.TYPE_CORPORATION,
+};
 
 function process(data) {
     dump("Processind wallet transaction data in wallet.js\n");
-    var rowsets, rowset;
+    var rows, row;
     var doc = data.wrappedJSObject.doc;
     var aux = data.wrappedJSObject.aux;
-/*
-    var replaceItemStm = this._conn.createStatement(
-            'replace into assets (id, typeID, owner, location, count, ' +
-            'flag, singleton, container) ' +
-            'values (:id, :type_id, :owner, :location, :count, :flag, :singleton, :container);'
-    );
+    var chid = aux.characterID;
+
+    let stm = gwm.storeTransactionStm;
     try {
-        rowsets = doc.evaluate('/eveapi/result/rowset', doc, null, 0, null);
+        rows = doc.evaluate("/eveapi/result/rowset/row", doc, null, 0, null);
     } catch (e) {
         dump("error in AssetsCallback:"+e.toString()+"\n");
     }
-    while (rowset = rowsets.iterateNext())
-        this._processAssetList(aux.wrappedJSObject.owner, rowset, replaceItemStm, doc);
-*/
+
+    while (row = rows.iterateNext())
+        try {
+            stm.params.char_id = chid;
+            stm.params.client = row.getAttribute("clientID");
+            stm.params.type_id = row.getAttribute("typeID");
+            stm.params.price = row.getAttribute("price");
+            stm.params.type = trTypeConversion[row.getAttribute("transactionType")];
+            stm.params.time = Date.UTCFromEveTimeString(row.getAttribute("transactionDateTime"));
+            stm.params.quantity = row.getAttribute("quantity");
+            stm.params.t_for = trTypeConversion[row.getAttribute("transactionFor")];
+            stm.execute();
+            stm.reset();
+            dump("got transaction: "+row.getAttribute("transactionType")+" "+
+                row.getAttribute("typeID")+" x"+row.getAttribute("quantity")+" on "+
+                row.getAttribute("transactionDateTime")+"\n");
+        } catch (e) {
+            dump("wallet process:" + e.toString()+"\n");
+            if (conn)
+                dump(conn.lastErrorString+"\n");
+        } finally {
+            stm.reset();
+        }
 }
 
 var components = [walletman, transaction];
@@ -160,4 +343,13 @@ if (XPCOMUtils.generateNSGetFactory)
     var NSGetFactory = XPCOMUtils.generateNSGetFactory(components);
 else
     var NSGetModule = XPCOMUtils.generateNSGetModule(components);
+
+Date.UTCFromEveTimeString = function (str) {
+    var d = str.split(/:| |-/);
+    d[1]--; // Month
+
+    return Date.UTC(d[0], d[1], d[2], d[3], d[4], d[5]);
+}
+
+transaction.fromStmRow = function (row) new transaction(row);
 
